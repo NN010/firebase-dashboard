796982360487fc028e406fed16d371798e320c5e
Nicholas Nguyen <nicholas.nguyen@shaw.ca>
2022-02-13 14:16:07 -0700

    First backup.

---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a dashboard I created using JavaScript and Firebase.
---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA21vZqrsPvFQjXrt_2LVMOZO6Ix2zN08o",
+  authDomain: "storefront-a83ac.firebaseapp.com",
+  databaseURL: "https://storefront-a83ac-default-rtdb.firebaseio.com",
+  projectId: "storefront-a83ac",
+  storageBucket: "storefront-a83ac.appspot.com",
+  messagingSenderId: "654741098991",
+  appId: "1:654741098991:web:4c32c8f448b406eff6e61a",
+  measurementId: "G-W3DMHKHS4K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
